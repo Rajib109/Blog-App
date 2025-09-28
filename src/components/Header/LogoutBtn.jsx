d3fcf370fe765f2f091f5fe9a9f3e82b9d934ff5
Shujaat Hussain <sujathussain426@gmail.com>
2025-09-28 23:00:56 +0530

    feat: refactor Header component and implement Logout button functionality

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/authSlice";
+import authService from "../../appwrite/auth";
+
+const Logout = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
+
+  return (
+    <button
+      onClick={handleLogout}
+      className="inline-block px-6 duration-200 hover:bg-blue-100 rounded-full"
+    >
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
-
-export {Header,Footer}
+import Container from './container/container'
+import Logo from './Logo'
+import LogoutBtn from './LogoutBtn/LogoutBtn'
+export {Header,Footer,Container,Logo,LogoutBtn}
