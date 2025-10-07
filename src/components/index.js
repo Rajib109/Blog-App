d559b08038e6a789ab84f8fcb32cdb56c40eb0c1
Shujaat Hussain <sujathussain426@gmail.com>
2025-10-07 22:57:18 +0530

    feat: update routing structure and integrate AuthLayout for protected routes

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,28 @@
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Container from './container/container'
-import Logo from './Logo'
-import Button from './Button'
-import Logout from './Header/LogoutBtn'
-import RTE from './RTE'
-import PostCard from './PostCard'
-import PostForm from './post-form/postForm'
-import Input from './Input'
-import Select from './Select'
-import Login from './Login'
-export {Header,Footer,Container,Logo,Button,Logout,RTE,PostCard,PostForm,Input,Select,Login}
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import Button from "./Button";
+import Logout from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/postForm";
+import Input from "./Input";
+import Select from "./Select";
+import Login from "./Login";
+import AuthLayout from "./AuthLayout";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Button,
+  Logout,
+  RTE,
+  PostCard,
+  PostForm,
+  Input,
+  Select,
+  Login,
+  AuthLayout,
+};
